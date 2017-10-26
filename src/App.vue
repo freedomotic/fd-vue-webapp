2e56748ce7f114d8277a83d5630ad74145a01c68
Mauro Cicolella <mcicolella@libero.it>
2017-10-26 10:00:12 +0000

    first adding of authentication - now it uses a fake auth

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,65 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <h1>Freedomotic Web Client</h1>
+    <ul>
+      <li>
+        <router-link v-if="loggedIn" to="/logout">Log out</router-link>
+        <router-link v-if="!loggedIn" to="/login">Log in</router-link>
+      </li>
+      <li>
+        <router-link to="/about">About</router-link>
+      </li>
+      <li>
+        <router-link to="/dashboard">Dashboard</router-link>
+        (authenticated)
+      </li>
+    </ul>
+    <template v-if="$route.matched.length">
+      <router-view></router-view>
+    </template>
+    <template v-else>
+      <p>You are logged {{ loggedIn ? 'in' : 'out' }}</p>
+    </template>
   </div>
 </template>
 
 <script>
+import auth from './auth'
 export default {
-  name: 'app'
+  data () {
+    return {
+      loggedIn: auth.loggedIn()
+    }
+  },
+  created () {
+    auth.onChange = loggedIn => {
+      this.loggedIn = loggedIn
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html, body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    color: #2c3e50;
+  }
+
+  #app {
+    padding: 0 20px;
+  }
+
+  ul {
+    line-height: 1.5em;
+    padding-left: 1.5em;
+  }
+
+  a {
+    color: #7f8c8d;
+    text-decoration: none;
+  }
+
+  a:hover {
+    color: #4fc08d;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import auth from '@/auth'
+import About from '@/components/About.vue'
+import Dashboard from '@/components/Dashboard.vue'
+import Login from '@/components/Login.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: __dirname,
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+    { path: '/about', component: About },
+    { path: '/dashboard', component: Dashboard, beforeEnter: requireAuth },
+    { path: '/login', component: Login },
+    { path: '/logout',
+      beforeEnter (to, from, next) {
+        auth.logout()
+        next('/')
+      }
     }
   ]
 })
+
+function requireAuth (to, from, next) {
+  if (!auth.loggedIn()) {
+    next({
+      path: '/login',
+      query: { redirect: to.fullPath }
+    })
+  } else {
+    next()
+  }
+}
