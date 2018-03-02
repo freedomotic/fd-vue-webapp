db0a3eb473f1b401b9ddebd579b14fb686013f9a
Mauro Cicolella <mcicolella@libero.it>
2018-03-02 10:10:22 +0100

    Updated address of online API demo

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"'
-  , FD_API_URL: '"http://104.236.92.231:9111/v3"'
+  , FD_API_URL: '"http://174.138.4.3:9111/v3"'
 })
