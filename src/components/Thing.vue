e18d59bff83e4a9340d6e6eefddeaa7cdb58a333
Mauro Cicolella <mcicolella@libero.it>
2018-03-16 17:39:26 +0000

    Some improvements to Thing and ThingEditor components

--- src/components/Thing.vue
+++ src/components/Thing.vue
@@ -4,17 +4,23 @@
       <md-card-header>
         <md-card-header-text>
           <div class="md-title">{{thing.name}}</div>
-          <div class="md-subhead">Normal size</div>
+          <div class="md-subhead">{{thing.type.split(/[. ]+/).pop()}}</div>
         </md-card-header-text>
 
         <md-card-media>
           <img src="../assets/plugin-running.png" alt="People">
         </md-card-media>
       </md-card-header>
+      
+      <div>
+        <md-content v-if="thing.type =='EnvObject.Thermometer' || thing.type =='EnvObject.Thermostat'">
+          {{thing.behaviors[0].value/thing.behaviors[0].scale}}
+        </md-content>
+      </div>
 
       <md-card-expand>
         <md-card-actions md-alignment="space-between">
-         <md-button class="md-icon-button" @click="showEditThingModal">
+         <md-button class="md-icon-button" @click="showThingsEditorModal">
             <md-icon>settings</md-icon>
          </md-button>
 
@@ -46,10 +52,15 @@ export default {
     ThingsEditor
   },
   methods: {
-    showEditThingModal () {
+    showThingsEditorModal () {
       this.$modal.show(ThingsEditor, {
         thing: this.thing
       }, {
+        adaptive: true,
+        resizable: true,
+        clickToClose: false,
+        width: '50%',
+        height: '60%'
       })
     }
   }
--- src/components/ThingsEditor.vue
+++ src/components/ThingsEditor.vue
@@ -7,31 +7,59 @@
           <h4 class="md-title">{{thing.name}}</h4>
     </md-toolbar>
     <div>
-      <vue-tabs active-tab-color="#e74c3c" 
-            active-text-color="white"
+     <form>    
+      <vue-tabs active-tab-color="#8bae2d" 
+            active-text-color="#ededed"
             type="pills"
             :start-index="1"
             direction="vertical"
       >
-       <v-tab title="Properties" icon="ti-user">
-        <div>UUID: {{thing.uuid}}</div>
-        <div>Name: {{thing.name}}</div>
-        <div>Description: {{thing.description}}</div>
-        <div>Protocol: {{thing.protocol}}</div>
-        <div>Address: {{thing.phisicalAddress}}</div>
-        <div>Tags: {{thing.tags}}</div>
+       <v-tab :title="$t('properties')">
+        <md-field>
+            <label>{{$t('uuid')}}</label>
+            <md-input v-model="uuid" readonly></md-input>
+        </md-field>
+        <md-field>
+            <label>{{$t('name')}}</label>
+            <md-input required v-model="name"></md-input>
+        </md-field>
+        <md-field>
+            <label>{{$t('description')}}</label>
+            <md-input v-model="description"></md-input>
+        </md-field>
+        <md-field>
+            <label>{{$t('protocol')}}</label>
+            <md-input v-model="protocol"></md-input>
+        </md-field>
+        <md-field>
+            <label>{{$t('address')}}</label>
+            <md-input v-model="address"></md-input>
+        </md-field>
+        <md-field>
+            <label>{{$t('tags')}}</label>
+            <md-input v-model="tags"></md-input>
+        </md-field>
+        <div id="properties-tab-buttons">
+           <md-button id="create-copy-button" class="md-raised">{{$t('create_copy')}}</md-button>
+           <md-button id="delete-thing-button" class="md-raised">{{$t('delete_thing')}}</md-button>
+        </div>
        </v-tab>
-      <v-tab title="Appearance" icon="ti-settings">
+      <v-tab :title="$t('appearance')">
         Second tab content
        </v-tab>
-       <v-tab title="Control panel" icon="ti-check">
-        Third tab content
+       <v-tab :title="$t('control_panel')">
+          <div v-for="behavior in thing.behaviors">
+            <span>{{behavior.name}}</span>
+            <span v-if="behavior['@class'] == 'com.freedomotic.model.object.RangedIntBehavior'">add slider</span>
+            <span v-if="behavior['@class'] == 'com.freedomotic.model.object.BooleanBehavior'">add switch</span>
+          </div> 
        </v-tab>
-       <v-tab title="Automations" icon="ti-check">
+       <v-tab :title="$t('automations')">
         Third tab content
        </v-tab>
       </vue-tabs>
-     </div>  
+     </form> 
+    </div>  
   </div>
 </template>
 
@@ -46,7 +74,24 @@ export default {
     VTab
   },
   computed: {},
-  data: () => ({}),
+  created () {
+    this.uuid = this.thing.uuid
+    this.name = this.thing.name
+    this.description = this.thing.description
+    this.protocol = this.thing.protocol
+    this.address = this.thing.physicalAddress
+    this.tags = this.thing.tags
+  },
+  data () {
+    return {
+      uuid: '',
+      name: '',
+      description: '',
+      protocol: '',
+      address: '',
+      tags: []
+    }
+  },
   methods: {}
 }
 </script>
