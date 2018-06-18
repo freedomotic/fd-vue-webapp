663a7f69e98a9b28ed46eb45c2b8d460130a1fd1
Mauro Cicolella <mcicolella@libero.it>
2018-06-18 10:53:37 +0200

    Enable API for Triggers retrieving

--- src/store/actions.js
+++ src/store/actions.js
@@ -72,9 +72,13 @@ export const getAutomationsList = (context) => {
 }
 
 export const getTriggersList = (context) => {
-  // mapped to  /triggers
-  // commit setTriggersList mutation
-  console.log('Executing getTriggersList')
+  axios.get('/triggers').then((response) => {
+    console.log('Retrieving Triggers list')
+    // commit setTriggersList mutation
+    context.commit('setTriggersList', { list: response.data })
+  }, (err) => {
+    console.log(err)
+  })
 }
 
 export const getUsersList = (context) => {
