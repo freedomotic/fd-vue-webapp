5db3177c3b2539f192030d6d1e5b5648f6ee3e3a
Mauro Cicolella <mcicolella@libero.it>
2018-06-25 17:47:05 +0200

    Add mobile Automations section

--- src/components/mobile/mobile-pages/Automations.vue
+++ src/components/mobile/mobile-pages/Automations.vue
@@ -1,27 +1,70 @@
 <template>
   <v-ons-page>
     <custom-toolbar v-bind="toolbarInfo"></custom-toolbar>
+    <v-ons-list>
      <ons-list-item v-for="automation in getAutomationsList" :key="automation.uuid" modifier="longdivider" tappable>
         <label class="center">
           {{ automation.shortDescription }}
         </label>
       </ons-list-item>
-    
+    </v-ons-list>
+    <v-ons-fab position="bottom right" @click="transition('default', null, true)">
+        <v-ons-icon  style="color: green;" icon="md-plus"></v-ons-icon>
+    </v-ons-fab>    
   </v-ons-page>
 </template>
 
 <script>
+const transitionPage = {
+  template: `
+    <v-ons-page v-if="addMode === false">
+      <custom-toolbar backLabel="Automations">
+        {{ automation.shortDescription }}
+      </custom-toolbar>
+      <mobile-automation :automation="automation" :addMode="addMode"></mobile-automation>
+    </v-ons-page>
+    <v-ons-page v-else>
+      <custom-toolbar backLabel="Automations">
+        Create new automation
+      </custom-toolbar>
+      <mobile-automation :automation="automation" :addMode="addMode"></mobile-automation>
+    </v-ons-page>
+    `
+}
+
 export default {
+  props: ['pageStack', 'setOptions'],
+  data () {
+    return {}
+  },
   computed: {
     getAutomationsList: function () {
       return this.$store.state.automationsList
-    },
-    data () {
-      return {}
     }
   },
   mounted () {
     this.$store.dispatch('getAutomationsList')
+  },
+  methods: {
+    transition (name, item, addMode) {
+      this.setOptions({
+        animation: name,
+        automation: item,
+        addMode: addMode,
+        callback: () => this.setOptions({})
+      })
+
+      this.pageStack.push({
+        extends: transitionPage,
+        data () {
+          return {
+            animation: name,
+            automation: item,
+            addMode: addMode
+          }
+        }
+      })
+    }
   }
 }
 </script>
