50bb7097cab61b74fc5941d658fea884dadb05c6
Mauro Cicolella <mcicolella@libero.it>
2018-06-26 17:24:49 +0200

    Fix Heroku login error

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@
 </template>
 
 <script>
-import axios from '@/utils/fd-axios'
 import Login from './components/Login.vue'
 import Dashboard from './components/Dashboard.vue'
 import MobileAppNavigator from './components/mobile/MobileAppNavigator.vue'
@@ -32,10 +31,6 @@ export default {
   created () {
     this.$store.isMobile = this.$ons.platform.isAndroid() || this.$ons.platform.isIOS()
     console.log('Created ' + this.$store.isMobile)
-    // used for online demo
-    if (window.location.href.includes('fd-vue-webapp.herokuapp.com')) {
-      axios.defaults.baseURL = process.env.FD_API_URL
-    }
   },
   computed: {
     loggedIn () {
