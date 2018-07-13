d88535715bd7b3bb1544d3bcc033d594a6c01e7f
Mauro Cicolella <mcicolella@libero.it>
2018-07-13 10:35:03 +0200

    Some improvements to mobile Thing component

--- src/components/mobile/mobile-pages/Thing.vue
+++ src/components/mobile/mobile-pages/Thing.vue
@@ -2,8 +2,65 @@
  <v-ons-page> 
   <div v-if="addMode === false">
    <v-ons-card>
-    <div>{{thing.name}}</div>
-  </v-ons-card>
+    <v-ons-list>
+      <v-ons-list-item>
+        <div class="center">
+          <v-ons-select style="width: 40%"
+            v-model="selectedSection"
+          >
+            <option v-for="section in sections" :value="section.value">
+              {{ $t(section.text) }}
+            </option>
+          </v-ons-select>
+        </div>
+      </v-ons-list-item>
+     </v-ons-list>
+    </v-ons-card>
+   <v-ons-card v-if="selectedSection ==='properties'">
+    <v-ons-list>
+     <v-ons-list-item>
+       <div class="center">
+         <label v-if="this.$ons.platform.isIOS()" for="UUID">UUID</label>
+         <v-ons-input input-id="UUID" v-model="UUID" placeholder="UUID" float>
+         </v-ons-input>
+     </div> 
+     </v-ons-list-item>
+     <v-ons-list-item>
+       <div class="center">
+         <label v-if="this.$ons.platform.isIOS()" for="Name">Name</label>
+         <v-ons-input input-id="Name" v-model="name" placeholder="Name" float>
+         </v-ons-input>
+       </div> 
+     </v-ons-list-item>
+     <v-ons-list-item>
+       <div class="center">
+         <label v-if="this.$ons.platform.isIOS()" for="Description">Description</label> 
+         <v-ons-input input-id="Description" v-model="description" placeholder="Description" float>
+         </v-ons-input>
+       </div>
+     </v-ons-list-item>
+     <v-ons-list-item>
+       <div class="center">
+         <label v-if="this.$ons.platform.isIOS()" for="Protocol">Protocol</label> 
+         <v-ons-input input-id="Protocol" v-model="protocol" placeholder="Protocol" float>
+         </v-ons-input>
+       </div>
+     </v-ons-list-item>
+     <v-ons-list-item>
+       <div class="center">
+         <label v-if="this.$ons.platform.isIOS()" for="Address">Address</label> 
+         <v-ons-input input-id="Address" v-model="address" placeholder="Address" float>
+         </v-ons-input>
+       </div>
+     </v-ons-list-item>
+     <v-ons-list-item>
+      <v-ons-button modifier="large">{{$t('create_copy')}}</v-ons-button>
+     </v-ons-list-item>
+     <v-ons-list-item>
+      <v-ons-button @click="deleteThing(thing.name, thing.uuid)" modifier="large">{{$t('delete_thing')}}</v-ons-button>   
+     </v-ons-list-item>
+    </v-ons-list>
+   </v-ons-card>
   </div> 
   <div v-else>
    <v-ons-list>
@@ -19,6 +76,9 @@
       </v-ons-list-item>
     </v-ons-list>
   </div>
+  <v-ons-fab position="bottom right">
+        <v-ons-icon icon="md-check"></v-ons-icon>
+  </v-ons-fab>
  </v-ons-page> 
 </template>
 
@@ -27,6 +87,24 @@
     props: {
       thing: {},
       addMode: Boolean
+    },
+    data () {
+      return {
+        sections: [
+          { text: 'properties', value: 'properties' },
+          { text: 'appearance', value: 'appearance' },
+          { text: 'data_source', value: 'data_source' },
+          { text: 'actions', value: 'actions' },
+          { text: 'control_panel', value: 'control_panel' },
+          { text: 'automations', value: 'automations' }
+        ],
+        selectedSection: 'properties',
+        UUID: this.thing.uuid,
+        name: this.thing.name,
+        description: this.thing.description,
+        protocol: this.thing.protocol,
+        address: this.thing.address
+      }
     },
     mounted () {
       this.$store.dispatch('getThingTemplatesList')
@@ -37,6 +115,23 @@
       }
     },
     methods: {
+      deleteThing: function (thingName, thingId) {
+        const self = this
+        this.$ons.notification.confirm({
+          title: 'Delete thing',
+          message: 'Do you want to delete "' + thingName + "'?",
+          buttonLabels: ['Cancel', 'Delete'],
+          callback: function (idx) {
+            switch (idx) {
+              case 0:
+                break
+              case 1:
+                self.$store.dispatch('deleteThing', thingId)
+                break
+            }
+          }
+        })
+      },
       addNewThing: function (template) {
         return this.$store.dispatch('addNewThing', template)
       }
