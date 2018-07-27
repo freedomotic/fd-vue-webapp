11efcef9e1e878831d7f078a61031ce2e8810c9b
Mauro Cicolella <mcicolella@libero.it>
2018-07-27 15:38:16 +0200

    Add User and Role components

--- src/components/RolesPanel.vue
+++ src/components/RolesPanel.vue
@@ -9,15 +9,29 @@
           </md-content> 
           <h3 class="md-title">{{$t('roles').toUpperCase()}}</h3>
         </md-toolbar>
-        
+        <grid-container>
+           <div class="item" v-for="(role, index) in getRolesList" :key="role.uuid">
+             <role :index="index"></role>
+           </div>
+        </grid-container>  
     </div>    
 </template>
 
 <script>
+import GridContainer from './common/GridContainer.vue'
+import Role from './Role.vue'
+
 export default {
+  components: {
+    Role,
+    GridContainer
+  },
   computed: {
     rolesOpen: function () {
       return this.$store.state.rolesOpen
+    },
+    getRolesList: function () {
+      return this.$store.state.rolesList
     }
   },
   mounted () {
@@ -44,27 +58,28 @@ export default {
         bottom: 1%;
         left: 1%;
         right: 1%;
-        background:white;
+        background: white;
         border-radius: 4px;
-        z-index: 130;
+        z-index: 10;
         color: black;
         display: flex;
-        align-items: center;
         flex-direction: column;
+        align-items: flex-start;
+        overflow-y: scroll;
         margin: auto;
       }
-
+ 
    #action-container {
        text-align: center;
        cursor: pointer;
        background-color: transparent;
        margin-top: 50%;
    }  
-
    .small-icon {
       width: 24px;
       margin: 1%;
       cursor: pointer;
       background:transparent;
-   } 
+   }
+   
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,81 @@
+<template>
+ <md-card>
+  <md-card-header>
+    <md-card-media>
+    </md-card-media>  
+  </md-card-header>
+      
+  <md-card-header>    
+    <md-card-header-text>
+      <div class="md-title">{{ getUserFromStore.name }}</div>
+      <div class="md-subhead">{{ getUserFromStore.roles }}</div>
+    </md-card-header-text>
+  </md-card-header>
+  <md-card-actions>
+     <md-button class="md-icon-button" >
+       <md-icon>settings</md-icon>
+     </md-button>
+     <md-button class="md-icon-button">
+       <md-icon>delete</md-icon>
+     </md-button>
+  </md-card-actions> 
+ </md-card>
+</template>
+
+<script>
+  export default {
+    props: {
+      index: ''
+    },
+    data () {
+      return {
+        image: ''
+      }
+    },
+    computed: {
+      getUserFromStore: function () {
+        return this.$store.state.usersList[this.index]
+      }
+    },
+    methods: {}
+  }
+</script>
+<style scoped>
+  .md-card .md-title {
+    font-size: 32px;
+    color: #ffffff;
+    letter-spacing: 0;
+    line-height: 28px;
+    text-align: center;
+   }
+   .md-card .md-subhead {
+    font-size: 14px;
+    color: #ffffff;
+    letter-spacing: 0;
+    line-height: 20px;
+    text-align: center;
+   }
+   
+   .md-theme-default.md-card {
+    width: 300px;
+    height: 300px;
+    background-color: #8bae2d;
+   }
+  .md-card-actions {
+    position: absolute;
+    height: 50px;
+    width: 100%;
+    bottom: 0;
+  }
+   .md-card .md-card-header .md-card-media {
+      width: 64px;
+      -ms-flex: 0 0 64px;
+          flex: 0 0 64px;
+      height: 64px;
+      margin: auto;
+    }
+    
+    .md-card .md-card-actions {
+      background-color: #ffffff;
+    }
+</style>
