01f3d51299ba3a5be3bcda8be5340b5597d2ada5
Mauro Cicolella <mcicolella@libero.it>
2018-08-01 09:38:04 +0200

    Introduce things inside an environment

--- src/components/Environment.vue
+++ src/components/Environment.vue
@@ -3,7 +3,13 @@
     <div style="z-index:1;">
        <h4 id="envname">{{environment.name}}</h4>
     </div>
-    <canvas ref='canvas' class='environment-canvas' width="700" height="550"/>    
+    <canvas ref='environmentCanvas' class='environment-canvas' width="700" height="550"/>
+    <div v-for="thing in getEnvironmentThingsList">
+      <img v-if="!moveEnabled" class="thing" :id="thing.uuid" src="../assets/icons/led-green.png"
+       :style="objPosition(thing.representation[0].offset)"></img>
+      <img v-else class="thing movable" :id="thing.uuid" src="../assets/icons/led-green.png"
+       :style="objPosition(thing.representation[0].offset)" draggable="true"></img>
+    </div>    
   </div>  
 </template>
 
@@ -22,6 +28,9 @@ export default {
   },
   data () {
     return {
+      showZones: false,
+      scaleFactor: 1.0,
+      moveEnabled: false,
       canvas: {},
       context: {},
       backgroundImg: new Image(this.environment.width, this.environment.height)
@@ -41,10 +50,40 @@ export default {
         var yOffset = newHeight < this.canvas.height ? ((this.canvas.height - newHeight) / 2) : 0
         this.context.drawImage(this.backgroundImg, xOffset, yOffset, newWidth, newHeight)
       }.bind(this), false)
+    },
+    objPosition: function (offset) {
+      console.log('left:' + offset.x + 'px;' + 'top:' + offset.y + 'px;')
+      return 'left:' + offset.x + 'px;' + 'top:' + offset.y + 'px;'
+    },
+    dragStartHandler: function (e) {
+      // set avatar icon
+      var dragInfo = e.detail
+      dragInfo.scaleFactor = this.scaleFactor
+      // dragInfo.moveService = this.$.moveService;
+      console.log(dragInfo)
+      if (dragInfo.event.path[0].attributes.draggable) {
+        dragInfo.avatar.style.cssText = 'border: 3px solid black; width: ' + 50 * this.scaleFactor + 'px; height:' + 50 * this.scaleFactor + 'px; border-radius: 0 50px 50px 50px; background-color: rgba(200,200,200,0.3)'
+        console.log(dragInfo.event.path[0].id)
+        dragInfo.drag = function () {}
+        dragInfo.drop = this.drop
+      }
+    },
+    drop: function (dragInfo) {
+      // move Thing to drop position
+      // var f = dragInfo.framed
+      // var thing = dragInfo.event.target.templateInstance.model.envObject
+      // var newPos = {
+      //  x: f.x / dragInfo.scaleFactor,
+      //  y: f.y / dragInfo.scaleFactor
+      // }
+      // POST updated data.
+      // var url = 'things/' + thing.uuid + '/move/' + Math.floor(newPos.x) + '/' + Math.floor(newPos.y)
+      //  dragInfo.moveService.fdtype = url;
+      //  dragInfo.moveService.go();
     }
   },
   mounted: function () {
-    this.canvas = this.$refs.canvas
+    this.canvas = this.$refs.environmentCanvas
     this.context = this.canvas.getContext('2d')
     this.backgroundImg.src = require('../assets/map2.png')
     this.backgroundImg.className = 'environment-image'
@@ -72,6 +111,12 @@ export default {
         position: absolute;
         z-index: 1000;
         transition: all 1s ease-in-out;
+      }
+   
+      .movable {
+        border: 3px solid black;
+        border-radius: 0 50px 50px 50px;
+        background-color: rgba(200, 200, 200, 0.3);
       }
       
       #envname {
