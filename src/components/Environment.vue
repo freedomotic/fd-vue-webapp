74c19494925d3159207567ff1cdd846e602fb896
Mauro Cicolella <mcicolella@libero.it>
2018-08-11 17:47:31 +0200

    Introduce v-tooltip

--- src/components/Environment.vue
+++ src/components/Environment.vue
@@ -6,7 +6,17 @@
     <canvas ref='environmentCanvas' class='environment-canvas' :width="environment.width" :height="environment.height"/>
     <div v-for="thing in getEnvironmentThingsList">
       <img v-if="!moveEnabled" class="thing" :id="thing.uuid" src="../assets/icons/led-green.png"
-       :style="objPosition(thing.representation[0].offset)" @contextmenu="openThingEditor(thing)" @click="sendClickEvent(thing.uuid)"></img>
+       :style="objPosition(thing.representation[0].offset)" @contextmenu="openThingEditor(thing)" @click="sendClickEvent(thing.uuid)"
+      v-tooltip="{
+       content: thing.name,
+       placement: 'right-start',
+       offset: 10,
+       delay: {
+        show: 500,
+        hide: 300,
+       },
+      }"
+      ></img>
       <img v-else class="thing movable" :id="thing.uuid" src="../assets/icons/led-green.png"
        :style="objPosition(thing.representation[0].offset)" draggable="true"></img>
     </div>    
@@ -15,7 +25,12 @@
 
 <script>
 /* global Image */
+import Vue from 'vue'
+import { VTooltip, VPopover, VClosePopover } from 'v-tooltip'
 import ThingsEditor from './ThingsEditor.vue'
+Vue.directive('tooltip', VTooltip)
+Vue.directive('close-popover', VClosePopover)
+Vue.component('v-popover', VPopover)
 
 export default {
   name: 'Environment',
@@ -197,6 +212,115 @@ export default {
         left: 15%;
         position: absolute;
       }
+
+      .tooltip {
+        display: block !important;
+        z-index: 10000;
+      }
+
+      .tooltip .tooltip-inner {
+        background: black;
+        color: white;
+        border-radius: 16px;
+        padding: 5px 10px 4px;
+      }
+
+      .tooltip .tooltip-arrow {
+        width: 0;
+        height: 0;
+        border-style: solid;
+        position: absolute;
+        margin: 5px;
+        border-color: black;
+      }
+
+      .tooltip[x-placement^="top"] {
+        margin-bottom: 5px;
+      }
+
+      .tooltip[x-placement^="top"] .tooltip-arrow {
+        border-width: 5px 5px 0 5px;
+        border-left-color: transparent !important;
+        border-right-color: transparent !important;
+        border-bottom-color: transparent !important;
+        bottom: -5px;
+        left: calc(50% - 5px);
+        margin-top: 0;
+        margin-bottom: 0;
+      }
+
+      .tooltip[x-placement^="bottom"] {
+        margin-top: 5px;
+      }
+
+      .tooltip[x-placement^="bottom"] .tooltip-arrow {
+        border-width: 0 5px 5px 5px;
+        border-left-color: transparent !important;
+        border-right-color: transparent !important;
+        border-top-color: transparent !important;
+        top: -5px;
+        left: calc(50% - 5px);
+        margin-top: 0;
+        margin-bottom: 0;
+      }
+
+      .tooltip[x-placement^="right"] {
+        margin-left: 5px;
+      }
+
+      .tooltip[x-placement^="right"] .tooltip-arrow {
+        border-width: 5px 5px 5px 0;
+        border-left-color: transparent !important;
+        border-top-color: transparent !important;
+        border-bottom-color: transparent !important;
+        left: -5px;
+        top: calc(50% - 5px);
+        margin-left: 0;
+        margin-right: 0;
+      }
+
+      .tooltip[x-placement^="left"] {
+        margin-right: 5px;
+      }
+
+      .tooltip[x-placement^="left"] .tooltip-arrow {
+        border-width: 5px 0 5px 5px;
+        border-top-color: transparent !important;
+        border-right-color: transparent !important;
+        border-bottom-color: transparent !important;
+        right: -5px;
+        top: calc(50% - 5px);
+        margin-left: 0;
+        margin-right: 0;
+      }
+
+      .tooltip[aria-hidden='true'] {
+        visibility: hidden;
+        opacity: 0;
+        transition: opacity .15s, visibility .15s;
+      }
+
+      .tooltip[aria-hidden='false'] {
+        visibility: visible;
+        opacity: 1;
+        transition: opacity .15s;
+      }
+
+      .info { 
+        $color: rgba(#004499, .9); 
+          .tooltip-inner { 
+             background: $color;
+             color: white;
+             padding: 24px;
+             border-radius: 5px;
+             box-shadow: 0 5px 30px rgba(black, .1);
+             max-width: 250px;
+           } 
+
+          .tooltip-arrow { 
+             border-color: $color;
+          }
+       }
       
 </style>
 
