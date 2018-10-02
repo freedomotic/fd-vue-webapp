41a0a30cc5d5c3b8b25f80b5d1fbb0b04961c513
Agustin Varea <agvarea@gmail.com>
2018-10-02 13:10:07 -0300

    Add Spanish language

--- src/main.js
+++ src/main.js
@@ -30,6 +30,7 @@ import nl from './i18n/nl_NL.json'
 import ro from './i18n/ro_RO.json'
 import ru from './i18n/ru_RU.json'
 import sk from './i18n/sk_SK.json'
+import sp from './i18n/sp_SP.json'
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
@@ -75,6 +76,7 @@ Vue.i18n.add('nl', nl)
 Vue.i18n.add('ro', ro)
 Vue.i18n.add('ru', ru)
 Vue.i18n.add('sk', sk)
+Vue.i18n.add('sp', sp)
 
 // Set the start locale to use
 Vue.i18n.set('en')
