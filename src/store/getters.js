5f6e4b3bec49f3af474dc5ae1df1d3a6e329afdd
Jeff Duke <jeff.duke@gmail.com>
2018-10-24 21:09:06 -0400

    add new getter for protocols

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,7 +2,12 @@ export const authStatus = (state) => {
   state.status
 }
 
-export const availableProtocols = (state) => {
-  console.log('availableProtocols getter')
+export const availableProtocols = state => {
+  const protocols = []
+  state.thingsList.filter(trigger => {
+    if (protocols.indexOf(trigger.protocol) === -1) {
+      protocols.push(trigger.protocol)
+    }
+  })
+  return protocols
 }
-
