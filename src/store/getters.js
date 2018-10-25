b3cbcbd42cc3d4bf8691be8f47279cd27c16bfea
Jeff Duke <jeff.duke@gmail.com>
2018-10-24 20:50:55 -0400

    Add a vuex getter to return triggers from the triggersList where hardwareLevel is true

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,15 @@
-export const authStatus = (state) => {
+export const authStatus = state => {
   state.status
 }
 
-export const availableProtocols = (state) => {
+export const availableProtocols = state => {
   console.log('availableProtocols getter')
 }
 
+export const getHardwareTriggers = state => {
+  return state.triggersList.filter(trigger => {
+    if (trigger.hardwareLevel === true) {
+      return trigger
+    }
+  })
+}
