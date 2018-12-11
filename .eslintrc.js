eb6216ad77827a5f9fee502b6e4e8cf433549206
atanda rasheed <atandarash@gmail.com>
2018-12-11 11:19:25 +0100

    Stash Previous Work Done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,46 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module'
   },
   env: {
-    browser: true,
+    browser: true
   },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue'
   ],
+  globals: {
+    'ga': true, // Google Analytics
+    'cordova': true,
+    '__statics': true
+  },
   // add your custom rules here
   'rules': {
+    // allow async-await
+    'generator-star-spacing': 'off',
+
     // allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
+    'one-var': 0,
+
+    'import/first': 0,
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
--- src/store/index.js
+++ src/store/index.js
@@ -1,54 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as actions from './actions'
-import * as mutations from './mutations'
-import * as getters from './getters'
+
+import example from './module-example'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-    // Authentication state
-    token: sessionStorage.getItem('token') || '',
-    status: '',
-    // end of Authentication state
-    // System state
-    systemInfo: {},
-    // end system state
-    // Dashboard state
-    isMobile: '',
-    displaySettings: false,
-    displayInfo: false,
-    displayAlerts: false,
-    displayLogout: false,
-    displayThings: false,
-    blur: false,
-    // end of Dashboard state
-    language: 'en',
-    // Settings state
-    systemOpen: false,
-    automationsOpen: false,
-    languagesOpen: false,
-    pluginsOpen: false,
-    rolesOpen: false,
-    usersOpen: false,
-    // end of Settings state
-    advancedMode: false,
-    // API state
-    automationsList: '',
-    commandsList: '',
-    environmentsList: '',
-    marketplaceCategoriesList: '',
-    marketplaceCategoryPluginsList: '',
-    pluginsList: [],
-    rolesList: '',
-    roomsList: '',
-    thingsList: [],
-    thingTemplatesList: '',
-    triggersList: [],
-    usersList: ''
-  },
-  actions,
-  mutations,
-  getters
-})
+/*
+ * If not building with SSR mode, you can
+ * directly export the Store instantiation
+ */
+
+export default function (/* { ssrContext } */) {
+  const Store = new Vuex.Store({
+    modules: {
+      example
+    }
+  })
+
+  return Store
+}
