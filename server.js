dd0d777314834c5aa740dc44e33f1d525ce1185f
Mauro Cicolella <mcicolella@libero.it>
2020-10-12 09:07:41 +0200

    Add dependencies for Heroku

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
 
-let app = express();
-app.use(serveStatic(__dirname + "/dist"));
+const app = express()
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Listening on port ' + port)
-});
+app.use(history())
+app.use(serveStatic(__dirname + '/dist'))
+app.listen(port)
